ee858f07a08122ac25f538fa298369046a1b8b1d
Galbatrollix <78318978+Galbatrollix@users.noreply.github.com>
2023-04-01 18:02:28 +0200

    rennamed tokenizer file to tokenization to avoid naming conflicts, train.py now loads tokenizer from file

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import utils
+import tokenization
 
 # required modules
 import pandas as pd
@@ -14,16 +15,24 @@ import string
 # nltk.download('averaged_perceptron_tagger') # only required for the first time
 # nltk.download('stopwords') # only required for the first time
 
+if __name__ == '__main__':
 
-TEST_SOLUTION_DATA_PATH = "data/test_data_solution.txt"
-TRAIN_DATA_PATH = "data/train_data.txt"
+    TEST_SOLUTION_DATA_PATH = "data/test_data_solution.txt"
+    TRAIN_DATA_PATH = "data/train_data.txt"
+    TOKENIZER_PATH = "tokenizers/tokenizerWP.pickle"
 
-# read train data set
-df_train = utils.load_data(TRAIN_DATA_PATH)
-print(df_train["genre"])
-print(df_train["genre"][0])
+    # read train data set
+    df_train = utils.load_data(TRAIN_DATA_PATH)
+    print(df_train["genre"])
+    print(df_train["genre"][0])
 
-# read test data set
-df_test = utils.load_data(TEST_SOLUTION_DATA_PATH)
-print(df_test.head())
+    # read test data set
+    df_test = utils.load_data(TEST_SOLUTION_DATA_PATH)
+    print(df_test.head())
 
+    #load tokenizer from file
+    tokenizer = tokenization.load_tokenizer(TOKENIZER_PATH)
+    outFull = tokenizer.encode_batch(df_test['description'])
+
+    print(len(outFull))
+    print(outFull[0].tokens)
