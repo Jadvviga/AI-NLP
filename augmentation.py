58fd165eaadd4ad0e5e5740e0b022565c09a1acd
jadvviga <ster.jadwiga@gmail.com>
2023-04-05 13:43:10 +0200

    cleaned up back translation aug

--- augmentation.py
+++ augmentation.py
@@ -1,49 +1,43 @@
 from transformers import MarianMTModel, MarianTokenizer
 
-# Get the name of the first model
-first_model_name = 'Helsinki-NLP/opus-mt-en-fr'
-# Get the tokenizer
-first_model_tkn = MarianTokenizer.from_pretrained(first_model_name)
-# Load the pretrained model based on the name
-first_model = MarianMTModel.from_pretrained(first_model_name)
+# EN-FR model + tokenizer
+en_fr_model_name = 'Helsinki-NLP/opus-mt-en-fr'
+en_fr_model_tkn = MarianTokenizer.from_pretrained(en_fr_model_name)
+en_fr_model = MarianMTModel.from_pretrained(en_fr_model_name)
 
-# Get the name of the second model
-second_model_name = 'Helsinki-NLP/opus-mt-fr-en'
-# Get the tokenizer
-second_model_tkn = MarianTokenizer.from_pretrained(second_model_name)
-# Load the pretrained model based on the name
-second_model = MarianMTModel.from_pretrained(second_model_name)
+# FR-EN model + tokenizer
+fr_en_model_name = 'Helsinki-NLP/opus-mt-fr-en'
+fr_en_model_tkn = MarianTokenizer.from_pretrained(fr_en_model_name)
+fr_en_model = MarianMTModel.from_pretrained(fr_en_model_name)
 
-original_texts = ["This article aims to perform the back translation for text data augmentation",
-          "It is the 25th article by Zoumana on Medium. He loves to give back to the community",
-          "The first model translates from English to French, which is a temporary process", 
-          "The second model finally translates back all the temporary french text into English"]
+en_lang, fr_lang = 'en', 'fr'
 
-def format_batch_texts(language_code, batch_texts):
-    formated_bach = [">>{}<< {}".format(language_code, text) for text in batch_texts]
-    return formated_bach
+original_texts = ["This article aims to perform the back translation for text data augmentation"]
 
-def perform_translation(batch_texts, model, tokenizer, language="fr"):
-    # Prepare the text data into appropriate format for the model
-    formated_batch_texts = format_batch_texts(language, batch_texts)
-    # Generate translation using model
-    translated = model.generate(**tokenizer(formated_batch_texts, return_tensors="pt", padding=True))
-    # Convert the generated tokens indices back into text
+def getTextsWithLangCode(language_code, texts):
+    texts_with_code = [">>{}<< {}".format(language_code, text) for text in texts]
+    return texts_with_code
+
+def translate(texts, model, tokenizer, language=fr_lang):
+    texts_with_code = getTextsWithLangCode(language, texts)
+    # translation with model + changing tokens bakc into text
+    translated = model.generate(**tokenizer(texts_with_code, return_tensors="pt", padding=True))
     translated_texts = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
+
     return translated_texts
 
-def combine_texts(original_texts, back_translated_batch):
-  return set(original_texts + back_translated_batch) 
-
-def perform_back_translation_with_augmentation(batch_texts, original_language="en", temporary_language="fr"):
-  # Translate from Original to Temporary Language
-  tmp_translated_batch = perform_translation(batch_texts, first_model, first_model_tkn, temporary_language)
-  # Translate Back to English
-  back_translated_batch = perform_translation(tmp_translated_batch, second_model, second_model_tkn, original_language)
-  # Return The Final Result
-  return combine_texts(original_texts, back_translated_batch)
 
 
-final_augmented = perform_back_translation_with_augmentation(original_texts)
-print(final_augmented)
+def augmentationBackTranslation(texts):
+  fr_translated_batch = translate(texts, en_fr_model, en_fr_model_tkn, fr_lang)
+  en_back_translated_batch = translate(fr_translated_batch, fr_en_model, fr_en_model_tkn, en_lang)
+  # combine texts
+  return set(original_texts + en_back_translated_batch) 
+
+
+augmented_texts = augmentationBackTranslation(original_texts)
+print(f"Original [{len(original_texts)}]: ")
+print(*original_texts, sep='\n')
+print(f"Augmented (Back Translation) [{len(augmented_texts)}]:")
+print(*augmented_texts, sep='\n')
 
