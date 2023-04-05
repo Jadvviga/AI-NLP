c9742ab6d2cb51ab73d090cc638364fbedb1c8a1
Galbatrollix <78318978+Galbatrollix@users.noreply.github.com>
2023-04-05 17:57:43 +0200

    made a file with stratified train data to 1500 made it possible to save to csv

--- augmentation.py
+++ augmentation.py
@@ -1,4 +1,5 @@
 from transformers import MarianMTModel, MarianTokenizer
+import random
 
 import utils
 
@@ -28,7 +29,7 @@ def getTextsWithLangCode(language_code, texts):
 def translate(texts, model, tokenizer, language=fr_lang):
     texts_with_code = getTextsWithLangCode(language, texts)
     # translation with model + changing tokens bakc into text
-    translated = model.generate(**tokenizer(texts_with_code, return_tensors="pt", padding=True),max_length=10000)
+    translated = model.generate(**tokenizer(texts_with_code, return_tensors="pt", padding=True), max_length=10000)
     translated_texts = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
 
     return translated_texts
@@ -40,12 +41,31 @@ def augmentationBackTranslation(texts):
     return en_back_translated_batch
 
 
+def save_to_csv(descriptions_dict, filename):
+    lines_to_csv = []
+    id_ = 0
+    for genre, list_of_descriptions in descriptions_dict.items():
+        for description in list_of_descriptions:
+            lines_to_csv.append(" ... ".join([str(id_),
+                                              "title",
+                                              genre,
+                                              description]))
+            id_ += 1
+
+    random.shuffle(lines_to_csv)
+    with open(filename, "w", encoding='utf-8') as outfile:
+        for line in lines_to_csv:
+            outfile.write(line)
+            outfile.write("\n")
+
+
+
 if __name__ == '__main__':
-    augmented_texts = augmentationBackTranslation(original_texts)
-    print(f"Original [{len(original_texts)}]: ")
-    print(*original_texts, sep='\n')
-    print(f"Augmented (Back Translation) [{len(augmented_texts)}]:")
-    print(*augmented_texts, sep='\n')
+    # augmented_texts = augmentationBackTranslation(original_texts)
+    # print(f"Original [{len(original_texts)}]: ")
+    # print(*original_texts, sep='\n')
+    # print(f"Augmented (Back Translation) [{len(augmented_texts)}]:")
+    # print(*augmented_texts, sep='\n')
 
     TRAIN_DATA_PATH = "data/train_data.txt"
     OUTPUT_DATA_PATH = "data/train_augmented_data.txt"
@@ -60,23 +80,23 @@ if __name__ == '__main__':
             dict_of_descriptions[genre] = []
         dict_of_descriptions[genre].append(df_train["description"][i])
 
-    #print(dict_of_descriptions["war"][0])
-    #print(augmentationBackTranslation([dict_of_descriptions['war'][0]]))
-    import time
-    now  = time.time()
-    print(*augmentationBackTranslation(dict_of_descriptions["war"]), sep='\n')
-    print(time.time() - now)
+    # print(dict_of_descriptions["war"][0])
+    # print(augmentationBackTranslation([dict_of_descriptions['war'][0]]))
+    # print(*augmentationBackTranslation(dict_of_descriptions["war"]), sep='\n')
+
     # cutting excessive entries in too big categories
-    """
+
     for key in dict_of_descriptions:
         # augmenting the categories with not enough entries
         if len(dict_of_descriptions[key]) <= TARGET_COUNT_PER_CATEGORY:
             new_list = list(dict_of_descriptions[key])
             while len(new_list) < TARGET_COUNT_PER_CATEGORY:
                 new_list.extend(augmentationBackTranslation(dict_of_descriptions[key]))
-        
+                #new_list.extend(dict_of_descriptions[key])
+            dict_of_descriptions[key] = new_list
         # cutting excess
         dict_of_descriptions[key] = dict_of_descriptions[key][0:TARGET_COUNT_PER_CATEGORY]
-    """
     for genre, list_ in dict_of_descriptions.items():
         print(f'{genre} : {len(list_)}')
+
+    save_to_csv(dict_of_descriptions, f"data/train_data_augmented_{TARGET_COUNT_PER_CATEGORY}.txt")
